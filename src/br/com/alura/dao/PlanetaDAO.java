25c714d1aa5f9572b3650962742af9d0cf2cfe32
João Victor <jvc.martins12@gmail.com>
2020-05-12 14:22:51 -0300

    Código da aula inicial - Requisição síncrona

--- src/br/com/alura/dao/PlanetaDAO.java
+++ src/br/com/alura/dao/PlanetaDAO.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
 public class PlanetaDAO {
@@ -12,7 +13,10 @@ public class PlanetaDAO {
 	public void listar() throws URISyntaxException, IOException, InterruptedException {
 		HttpClient httpClient = HttpClient.newBuilder().build();
 
-		httpClient.sendAsync(HttpRequest.newBuilder().uri(new URI("http://localhost:8080/planetas")).GET().build(),
-				BodyHandlers.ofString()).whenComplete((s, t) -> System.out.println(s.body()));
+		HttpResponse<String> send = httpClient.send(
+				HttpRequest.newBuilder().uri(new URI("http://localhost:8080/planetas")).GET().build(),
+				BodyHandlers.ofString());
+
+		System.out.println(send.body());
 	}
 }
